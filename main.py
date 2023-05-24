72dcc3129fbd4b2105064fd8bf37eb451cd6a9b2
joao vitor <joaovitorluz950@gmail.com>
2023-05-24 20:36:05 -0300

    alteracao 1

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 from funcoes import limp
+import time
+while True:
+    limp()
+    print("(0) Sair")
+    print("(1) Incluir aluno")
+    print("(2)Mostrar Lista")
+    print("(3)Deletar os dados")
+    opcao = input()
+    if opcao == "0":
+        break
+    elif opcao == "1":
+        
+    
 
+
+
+
+if opcao == 0:
+    break
 
 
 
